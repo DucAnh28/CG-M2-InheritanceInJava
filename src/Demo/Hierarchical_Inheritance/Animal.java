f69a08813eb45de8a61f28188b564a4e67cf3156
Duc Anh Nguyen <ducanhnguyeny2k@gmail.com>
2022-07-08 07:41:07 +0700

    update demo

---
+++ src/Demo/Hierarchical_Inheritance/Animal.java
@@ -0,0 +1,29 @@
+package Demo.Hierarchical_Inheritance;
+
+public class Animal {
+    void eat() {
+        System.out.println("eating...");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("barking...");
+    }
+}
+
+class Cat extends Animal {
+    void meow() {
+        System.out.println("meowing...");
+    }
+}
+
+class TestInheritance4 {
+    public static void main(String args[]) {
+        Cat c = new Cat();
+        c.meow();
+        c.eat();
+        // c.bark(); // Sẽ bị lỗi vì cat không kế thừa của dog
+    }
+}
+
