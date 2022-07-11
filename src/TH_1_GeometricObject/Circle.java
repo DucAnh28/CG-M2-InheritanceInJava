3867d29f76c21796dffab79995179ff02d4a781c
DucAnh28 <ducanhnguyeny2k@gmail.com>
2022-07-11 10:47:37 +0700

    update TH1

--- src/TH_1_GeometricObject/Circle.java
+++ src/TH_1_GeometricObject/Circle.java
@@ -3,4 +3,38 @@ package TH_1_GeometricObject;
 public class Circle extends Shape {
     static double radius;
 
+    public Circle(){}
+
+    public Circle(double radius){
+        this.radius = radius;
+    }
+
+    public Circle(double radius,String color,boolean filled){
+        super(color, filled);
+        this.radius = radius;
+    }
+
+    public static double getRadius() {
+        return radius;
+    }
+
+    public static void setRadius(double radius) {
+        Circle.radius = radius;
+    }
+
+    public double getArea(){
+        return radius* radius* Math.PI;
+    }
+
+    public double getPerimeter(){
+        return 2* radius* Math.PI;
+    }
+
+    @Override
+    public String toString() {
+            return "A Circle with radius="
+                    + getRadius()
+                    + ", which is a subclass of "
+                    + super.toString();
+        }
 }
